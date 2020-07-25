785207b8c587037472d84166b41c43f649db70da
GregorioSMC <48391784+GregorioSMC@users.noreply.github.com>
2020-07-24 19:06:25 -0500

    Control-Dynamixel-X28

---
+++ Falcon_0_0/Falcon_0_0/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Falcon_0_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnComenzar = new System.Windows.Forms.Button();
+            this.btnRutina = new System.Windows.Forms.Button();
+            this.btnRutina2 = new System.Windows.Forms.Button();
+            this.btnStarR1 = new System.Windows.Forms.Button();
+            this.btnStopR1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPanTill = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnComenzar
+            // 
+            this.btnComenzar.Location = new System.Drawing.Point(68, 61);
+            this.btnComenzar.Name = "btnComenzar";
+            this.btnComenzar.Size = new System.Drawing.Size(102, 31);
+            this.btnComenzar.TabIndex = 0;
+            this.btnComenzar.Text = "Comenzar";
+            this.btnComenzar.UseVisualStyleBackColor = true;
+            this.btnComenzar.Click += new System.EventHandler(this.btnComenzar_Click);
+            // 
+            // btnRutina
+            // 
+            this.btnRutina.Location = new System.Drawing.Point(68, 115);
+            this.btnRutina.Name = "btnRutina";
+            this.btnRutina.Size = new System.Drawing.Size(102, 31);
+            this.btnRutina.TabIndex = 1;
+            this.btnRutina.Text = "Rutina";
+            this.btnRutina.UseVisualStyleBackColor = true;
+            this.btnRutina.Click += new System.EventHandler(this.btnRutina_Click);
+            // 
+            // btnRutina2
+            // 
+            this.btnRutina2.Location = new System.Drawing.Point(68, 163);
+            this.btnRutina2.Name = "btnRutina2";
+            this.btnRutina2.Size = new System.Drawing.Size(102, 31);
+            this.btnRutina2.TabIndex = 2;
+            this.btnRutina2.Text = "Rutina 2";
+            this.btnRutina2.UseVisualStyleBackColor = true;
+            this.btnRutina2.Click += new System.EventHandler(this.btnRutina2_Click);
+            // 
+            // btnStarR1
+            // 
+            this.btnStarR1.Location = new System.Drawing.Point(254, 61);
+            this.btnStarR1.Name = "btnStarR1";
+            this.btnStarR1.Size = new System.Drawing.Size(90, 31);
+            this.btnStarR1.TabIndex = 3;
+            this.btnStarR1.Text = "Start";
+            this.btnStarR1.UseVisualStyleBackColor = true;
+            this.btnStarR1.Click += new System.EventHandler(this.btnStarR1_Click);
+            // 
+            // btnStopR1
+            // 
+            this.btnStopR1.Location = new System.Drawing.Point(254, 115);
+            this.btnStopR1.Name = "btnStopR1";
+            this.btnStopR1.Size = new System.Drawing.Size(90, 31);
+            this.btnStopR1.TabIndex = 4;
+            this.btnStopR1.Text = "Stop";
+            this.btnStopR1.UseVisualStyleBackColor = true;
+            this.btnStopR1.Click += new System.EventHandler(this.btnStopR1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPanTill
+            // 
+            this.btnPanTill.Location = new System.Drawing.Point(68, 200);
+            this.btnPanTill.Name = "btnPanTill";
+            this.btnPanTill.Size = new System.Drawing.Size(102, 31);
+            this.btnPanTill.TabIndex = 6;
+            this.btnPanTill.Text = "Pan-Till";
+            this.btnPanTill.UseVisualStyleBackColor = true;
+            this.btnPanTill.Click += new System.EventHandler(this.btnPanTill_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(254, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnPanTill);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStopR1);
+            this.Controls.Add(this.btnStarR1);
+            this.Controls.Add(this.btnRutina2);
+            this.Controls.Add(this.btnRutina);
+            this.Controls.Add(this.btnComenzar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnComenzar;
+        private System.Windows.Forms.Button btnRutina;
+        private System.Windows.Forms.Button btnRutina2;
+        private System.Windows.Forms.Button btnStarR1;
+        private System.Windows.Forms.Button btnStopR1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPanTill;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Falcon_0_0/Falcon_0_0/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ROBOTIS;
+
+namespace Falcon_0_0
+{
+    public partial class Form1 : Form
+    {
+        public const int DEFAULT_PORTNUM = 20;
+        public const int DEFAULT_BAUDRATE = 1;
+        public const int auxR1 = 4096;
+        public const int aux2R1 = 1296;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dynamixel.dxl_initialize(DEFAULT_PORTNUM, DEFAULT_BAUDRATE);
+
+        }
+
+        private void btnComenzar_Click(object sender, EventArgs e)
+        {
+            Iniciar();
+        }
+
+
+        public static void p1()
+        {
+            int j = 0;
+
+            for (int i = 2048; i < 2349; i++)
+            {
+                dynamixel.dxl_write_word(14, 30, i);
+                dynamixel.dxl_write_word(16, 30, i);
+                dynamixel.dxl_write_word(19, 30, i);
+                j = auxR1 - i;
+                dynamixel.dxl_write_word(18, 30, j);
+                dynamixel.dxl_write_word(20, 30, j);
+                dynamixel.dxl_write_word(13, 30, j);
+                dynamixel.dxl_write_word(15, 30, j);
+
+            }
+
+            j = 0;
+
+            for (int i = 1748; i < 2049; i++)
+            {
+                dynamixel.dxl_write_word(18, 30, i);
+                dynamixel.dxl_write_word(20, 30, i);
+                dynamixel.dxl_write_word(13, 30, i);
+                dynamixel.dxl_write_word(15, 30, i);
+                j = auxR1 - i;
+                dynamixel.dxl_write_word(14, 30, j);
+                dynamixel.dxl_write_word(16, 30, j);
+                dynamixel.dxl_write_word(19, 30, j);
+            }
+        }
+
+
+        public static void aux_()
+        {
+            int cont = 0;
+            int pos_obj =150;
+            int h = 2048;
+            int i = 2048;
+            int j = 2048;
+            int aux = 0;
+            int k = 2048;
+
+            while (cont < pos_obj)
+            {
+                cont++;
+                h --;
+                i ++;
+                j += 2;              
+                dynamixel.dxl_write_word(21, 30, k);
+                dynamixel.dxl_write_word(13, 30, h);
+                dynamixel.dxl_write_word(19, 30, i);
+                dynamixel.dxl_write_word(15, 30, j);                
+            }
+
+
+            cont = 0;
+
+            while (cont < pos_obj)
+            {
+                cont++;
+                h ++;
+                i --;
+                j -= 2;              
+                dynamixel.dxl_write_word(21, 30, k);
+                dynamixel.dxl_write_word(21, 30, 2048);
+                dynamixel.dxl_write_word(13, 30, h);
+                dynamixel.dxl_write_word(19, 30, i);
+                dynamixel.dxl_write_word(15, 30, j);
+            }
+        }
+
+        public static void r1IZ()
+        {
+            int j = 0;
+
+            for (int i = 2048; i < 2249; i++)
+            {
+                
+                dynamixel.dxl_write_word(15, 30, i);
+                dynamixel.dxl_write_word(19, 30, i);
+                j = auxR1 - i;
+                dynamixel.dxl_write_word(13, 30, j);
+                
+                
+            }
+
+            j = 0;
+
+            for (int i = 1848; i < 2049; i++)
+            {                
+                dynamixel.dxl_write_word(13, 30, i);
+                j = auxR1 - i;
+                dynamixel.dxl_write_word(15, 30, j);
+                dynamixel.dxl_write_word(19, 30, j);
+            }
+
+        }
+
+
+        public static void r1DE()
+        {
+            int j = 0;
+
+            for (int i = 2048; i < 2249; i++)
+            {
+
+                
+
+                dynamixel.dxl_write_word(19, 30, i);                
+                j = auxR1 - i;
+                dynamixel.dxl_write_word(13, 30, j);
+                dynamixel.dxl_write_word(15, 30, j);
+
+                
+
+            }
+
+            j = 0;
+
+            for (int i = 1848; i < 2049; i++)
+            {
+                dynamixel.dxl_write_word(13, 30, i);
+                dynamixel.dxl_write_word(15, 30, i);
+                j = auxR1 - i;
+                dynamixel.dxl_write_word(19, 30, j);               
+            }
+
+        }
+
+        public static void pan_till()
+        {
+
+            for (int i = 2048; i <= 2298; i++)
+            {
+                dynamixel.dxl_write_word(1,30,i);
+                dynamixel.dxl_write_word(2, 30, i);
+            }
+
+            for (int i = 2298; i <= 2298; i++)
+            {
+                dynamixel.dxl_write_word(1, 30, i);
+                dynamixel.dxl_write_word(2, 30, i);
+            }
+
+
+        }
+
+        public static void r2()
+        {
+            for (int i = 2048; i < 2200; i++)
+            {
+                dynamixel.dxl_write_word(20,30,i);
+                Thread.Sleep(20);
+            }
+
+            for (int i = 2200; i > 2047; i--)
+            {
+                dynamixel.dxl_write_word(20, 30, i);
+                Thread.Sleep(20);
+            }
+        }
+
+
+        public void Iniciar()
+        {          
+            for (int i =1 ; i <= 22; i++)
+            {
+                    dynamixel.dxl_write_word(i, 30, 2048);                
+            }
+
+
+
+        }
+
+        private void btnRutina_Click(object sender, EventArgs e)
+        {
+            //r1DE();
+            r1IZ();
+        }
+
+        private void btnRutina2_Click(object sender, EventArgs e)
+        {
+            aux_();
+        }
+
+        private void tmFBDE_Tick(object sender, EventArgs e)
+        {
+            r1DE();
+        }
+
+        private void tmFBIZ_Tick(object sender, EventArgs e)
+        {
+            r1IZ();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //tmFBIZ.Enabled = false;
+            //tmFBDE.Enabled = false;
+        }
+
+        private void btnStarR1_Click(object sender, EventArgs e)
+        {
+            //tmFBIZ.Enabled = true;
+           // tmFBDE.Enabled = true;
+        }
+
+        private void btnStopR1_Click(object sender, EventArgs e)
+        {
+            //tmFBIZ.Enabled = false;
+           // tmFBDE.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p1();
+        }
+
+        private void btnPanTill_Click(object sender, EventArgs e)
+        {
+            pan_till();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pan_till();
+        }
+    }
+}
